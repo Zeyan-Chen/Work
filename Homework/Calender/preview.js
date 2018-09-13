5be864ec301f0ee27f9745bf23b54eb60085c03d
LeoYan <leoyanisme@gmail.com>
2018-09-13 09:48:11 +0800

    Upload 9/13 Calendar

--- Homework/Calender/preview.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Calendar from "./components/Calendar.jsx";
-
-let data = {};
-
-ReactDOM.render(<Calendar />, document.getElementById("calender"));
