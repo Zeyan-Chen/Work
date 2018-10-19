1d5253a303bc3365e77025747a61447be70e40fe
LeoYan <leoyanisme@gmail.com>
2018-10-19 21:14:31 +0800

    restructor

---
+++ Training/v3/demo03/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './main.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // 正規表達式
+        exclude: /node_modules/, // 排除掉
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  }
+};
